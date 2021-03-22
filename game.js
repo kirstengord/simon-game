1a487987fca22cd81e46191d22a9c1e3659ac4ca
K Gord <75246798+kngord88@users.noreply.github.com>
2021-03-22 21:09:12 +0000

    Update game.js

--- game.js
+++ game.js
@@ -1,11 +1,9 @@
 var buttonColours = ["red", "blue", "green", "yellow"];
 
 var gamePattern = [];
-
 var userClickedPattern = [];
 
 var started = false;
-
 var level = 0;
 
 $(document).keypress(function() {
